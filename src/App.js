9d41df11a8cf2d8499ab4084971a2e2bc5376700
Akoth Cathy <catherinececilia22@gmail.com>
2022-02-14 04:38:34 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import Favorites from './pages/Favorites';
+import AllMeetups from './pages/AllMeetups';
+import NewMeetup from './pages/NewMeetup';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1> Cathy is hot.</h1>
+      <Route path = '/'>
+        <AllMeetups />
+      </Route>
+      <Route path = '/NewMeetup'>
+        <NewMeetup />
+      </Route>
+      <Route path = '/Favorites'>
+        <Favorites />
+      </Route>
     </div>
   );
 }
