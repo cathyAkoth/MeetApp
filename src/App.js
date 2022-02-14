638817a883f5ed22bf44f4d12d24ef720e7a9138
Akoth Cathy <catherinececilia22@gmail.com>
2022-02-14 11:43:34 -0800

    Added MainNavigation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Favorites from './pages/Favorites';
 import AllMeetups from './pages/AllMeetups';
 import NewMeetup from './pages/NewMeetup';
 import { Route , Switch} from 'react-router-dom';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
       <Route path = '/' exact>
         <AllMeetups />
